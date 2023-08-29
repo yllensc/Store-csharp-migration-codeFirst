5066fb4e4c67e376f4b2093c2ff21eaa61563d6c
yllensc <yllensc18@gmail.com>
2023-08-29 16:52:17 -0500

    Método put y post, check

--- API/Controllers/CountryController.cs
+++ API/Controllers/CountryController.cs
@@ -2,6 +2,7 @@ using API.Dtos;
 using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
+using Infrastructure.UnitOfWork;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -10,48 +11,67 @@ namespace API.Controllers
     {
         private readonly IUnitOfWork unitOfWork;
         private readonly IMapper mapper;
-    public CountryController(IUnitOfWork _unitOfWork, IMapper _mapper)
-    {
-        this.unitOfWork = _unitOfWork;
-        this.mapper = _mapper;
-    }
-    
-    //Definiciones HTTP 
-    //Get con DTO
-    [HttpGet]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    public async Task<ActionResult<IEnumerable<CountryDto>>> Get()
-    {
-        var countries = await unitOfWork.Countries.GetAllAsync();
-        return mapper.Map<List<CountryDto>>(countries);
-    }
-
-    [HttpGet("{id}")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public async Task<ActionResult<CountryDto>> Get(int id)
-    {
-        var country = await unitOfWork.Countries.GetByIdAsync(id);
-        if (country == null){
-            return NotFound();
+        public CountryController(IUnitOfWork _unitOfWork, IMapper _mapper)
+        {
+            this.unitOfWork = _unitOfWork;
+            this.mapper = _mapper;
         }
-        return this.mapper.Map<CountryDto>(country);    
-    }
 
-    [HttpPost]
+        //Definiciones HTTP 
+        //Get con DTO
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<IEnumerable<CountryDto>>> Get()
+        {
+            var countries = await unitOfWork.Countries.GetAllAsync();
+            return mapper.Map<List<CountryDto>>(countries);
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<CountryDto>> Get(int id)
+        {
+            var country = await unitOfWork.Countries.GetByIdAsync(id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+            return this.mapper.Map<CountryDto>(country);
+        }
+        //Método POST con DTO
+        [HttpPost]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task <ActionResult<Country>> Post(Country country)
+        public async Task<ActionResult<Country>> Post(CountryDto countryDto)
         {
-            unitOfWork.Countries.Add(country);
+            var country = this.mapper.Map<Country>(countryDto);
+            this.unitOfWork.Countries.Add(country);
             await unitOfWork.SaveAsync();
-            if(country == null)
+            if (country == null)
             {
                 return BadRequest();
             }
-            return CreatedAtAction(nameof(Post), new {id = country.Id}, country);
+            countryDto.Id = countryDto.Id;
+            return CreatedAtAction(nameof(Post), new { id = countryDto.Id }, countryDto);
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<CountryDto>> Put(int id, [FromBody]CountryDto countryDto){
+            if(countryDto == null)
+                return NotFound();
+            var country = this.mapper.Map<Country>(countryDto);
+            unitOfWork.Countries.Update(country);
+            await unitOfWork.SaveAsync();
+            return countryDto;
+            
         }
     }
+
+
 }
